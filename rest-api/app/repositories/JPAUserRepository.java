abd652e1570fee52f7962eae8d478a560d40f082
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-02-08 13:37:30 +0100

    Implemented findByUsername repository method

--- rest-api/app/repositories/JPAUserRepository.java
+++ rest-api/app/repositories/JPAUserRepository.java
@@ -6,12 +6,25 @@ import play.db.jpa.JPAApi;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.persistence.TypedQuery;
 import java.util.UUID;
+import java.util.concurrent.CompletionStage;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 @Singleton
 public class JPAUserRepository extends JPARepository<User, UUID> implements UserRepository {
     @Inject
     public JPAUserRepository(JPAApi jpaApi, DatabaseExecutionContext executionContext) {
         super(jpaApi, executionContext, User.class);
+    }
+
+    @Override
+    public CompletionStage<User> findByUsername(String username) {
+        return supplyAsync(() -> with(em -> {
+            TypedQuery<User> query = em.createQuery("select u from User u where username=:username", User.class);
+            query.setParameter("username", username);
+            return query.getSingleResult();
+        }));
     }
 }
--- rest-api/app/repositories/UserRepository.java
+++ rest-api/app/repositories/UserRepository.java
@@ -2,5 +2,8 @@ package repositories;
 
 import models.User;
 import java.util.UUID;
+import java.util.concurrent.CompletionStage;
 
-public interface UserRepository extends Repository<User, UUID> {}
+public interface UserRepository extends Repository<User, UUID> {
+    CompletionStage<User> findByUsername(String username);
+}

78172e47a2fc20135124784b3a4f548687ee9213
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-02-14 07:13:49 +0100

    Implemented Register component page with state

--- auction/src/components/Register.js
+++ auction/src/components/Register.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+import TextField from './Controls/TextField';
+import Label from './Controls/Label';
+import Button from './Controls/Button';
+import FormGroup from './Controls/FormGroup';
+import Container from './Common/Container';
 
 const Register = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <Container title='REGISTER'>
+            <FormGroup>
+                <Label isFor='firstName' label='First Name' />
+                <TextField onChange={e => setFirstName(e.target.value)} name='firstName' placeholder='e.g. John' fullWidth />
+            </FormGroup>
+
+            <FormGroup>
+                <Label isFor='lastName' label='Last Name' />
+                <TextField onChange={e => setLastName(e.target.value)} name='lastName' placeholder='e.g. Doe' fullWidth />
+            </FormGroup>
+
+            <FormGroup>
+                <Label isFor='email' label='Email' />
+                <TextField onChange={e => setEmail(e.target.value)} name='email' placeholder='e.g. johndoe@mail.com' fullWidth />
+            </FormGroup>
+
+            <FormGroup>
+                <Label isFor='password' label='Password' />
+                <TextField onChange={e => setPassword(e.target.value)} name='password' fullWidth password />
+            </FormGroup>
+
+            <FormGroup>
+                <Button 
+                    onClick={() => console.log(`First name: ${firstName}\nLast name: ${lastName}\nemail: ${email}\npassword: ${password}`)}
+                    type='primary' 
+                    fullWidth>
+                        Register
+                </Button>
+            </FormGroup>
+
+            <FormGroup flex around>
+                <span>
+                    Already have an account? <Link to='/login'>Login</Link>
+                </span>
+            </FormGroup>
+        </Container>
     )
 }
 

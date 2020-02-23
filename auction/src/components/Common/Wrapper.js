2a80a88a61677dbb6955728b34e361a29f8b4aba
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-02-23 23:17:50 +0100

    Updated Wrapper component

--- auction/src/components/Common/Wrapper.js
+++ auction/src/components/Common/Wrapper.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import classNames from 'classnames';
 
-const Wrapper = ({ children, flex, around }) => {
+const Wrapper = ({ children, flex, around, normal }) => {
     const classes = classNames(
         'wrapper',
         { 'flex': flex },
-        { 'flex-around': around }
+        { 'flex-around': around },
+        { 'flex-normal': normal }
     );
 
     return (

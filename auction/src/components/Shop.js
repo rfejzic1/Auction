3a08bc449edc7bf53d023ad7ecbd0e1293b28405
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-02-25 14:06:16 +0100

    Updated shop component

--- auction/src/components/Shop.js
+++ auction/src/components/Shop.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 import PageLayout from './PageLayout'
 import Wrapper from './Common/Wrapper';
 import SubcategoryList from './Home/SubcategoryList';
+import config from '../config';
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
 
+const getProducts = async ({ category, subcategory }, setProducts) => {
+    try {    
+        const res = await axios({
+            baseURL: config.API_URL,
+            url: `/products`,
+            params: {
+                category,
+                subcategory
+            }
+        })
+
+        setProducts(res.data);
+    } catch {
+        setProducts([]);
+    }
+};
+
 const Shop = () => {
     const query = useQuery();
-    console.log(query.get("category"));
-    
+    const categorization = {
+        category: query.get("category").slice(1,-1),
+        subcategory: query.get("subcategory")
+    };
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getProducts(categorization, setProducts);
+    }, [categorization]);
+
     return (
         <PageLayout>
             <Wrapper>
                 <SubcategoryList defaultCategory={query.get("category")}/>
+                <ul>
+                    { products.map((product, index) => {
+                        return <li key={index}>{product.name}</li>
+                    }) }
+                </ul>
             </Wrapper>
         </PageLayout>
     )

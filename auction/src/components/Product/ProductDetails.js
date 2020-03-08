bdcee6e52c7d6072e588e410e5e294785ba50a1b
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-08 19:57:07 +0100

    Implemented bidding

--- auction/src/components/Product/ProductDetails.js
+++ auction/src/components/Product/ProductDetails.js
@@ -13,40 +13,76 @@ const getBids = async (productID, setBids) => {
             baseURL: config.API_URL,
             url: `/products/${productID}/bids`
         });
-        console.log(res.data);
         setBids(res.data);
     } catch (e) {
         console.log(e);
     }
 };
 
+const placeBid = async (productID, value) => {
+    try {
+        await axios({
+            baseURL: config.API_URL,
+            url: `/products/${productID}/bids`,
+            method: 'POST',
+            data: {
+                value: Number(value)
+            }
+        });
+    } catch (e) {
+        if(e.response.data.status === 422) {
+            alert('Bid is not the highest!');
+        } else {
+            console.log(e.response);
+        }
+    }
+};
+
 const ProductDetails = ({ product }) => {
     const [bids, setBids] = useState([]);
+    const [value, setValue] = useState(0);     
+    
     const startPrice = product.startPrice;
-    const highestBid = (bids[0] && bids[0].value.toFixed(2)) || startPrice;
-    const numberOfBids = bids.length;
-
+    const highestBid = (bids[0] && bids[0].value.toFixed(2));
+    
     useEffect(() => {
-        getBids(product.uuid, setBids);
-    }, [product.uuid]);
+        const update = () => getBids(product.uuid, setBids);
+        const updateInterval = setInterval(update, config.POLLING_TIME);
 
+        update();
+
+        return () => clearInterval(updateInterval);
+    }, [product.uuid]);
+    
     const displayDate = milis => {
-        return new Date(milis).toUTCString();
+        return new Date(milis).toString();
+    };
+
+    const handleValueUpdate = e => {
+        setValue(e.target.value);
+    }
+
+    const handleBid = () => {
+        placeBid(product.uuid, value);
     };
 
     return (
         <div className='product-details'>
             <h2>{product.name}</h2>
-            <p className='price'>Start's from ${startPrice.toFixed(2)}</p>
+            <p className='price'>Starts at ${startPrice.toFixed(2)}</p>
             <br/>
-            <TextField />
-            <BidButton text='Place Bid' onClick={() => alert('You bid!')} className='outlined-primary uppercase' product={product} />
+            <TextField onChange={handleValueUpdate} />
+            <BidButton text='Place Bid' onClick={handleBid} className='outlined-primary uppercase' product={product} />
             <br/>
-            <span className='bid-info'>Enter ${highestBid} or more</span>
+            <span className='bid-info'>
+                {
+                    bids.length > 0 ? `Enter more than $${highestBid}` : `Enter $${highestBid || startPrice} or more`
+                }
+            </span>
             <FormGroup>
-                <span className='bid-info'>Highest bid: <span className='primary-bold' >${highestBid}</span></span>
+                <span className='bid-info'>Highest bid: <span className='primary-bold' >{highestBid ? `$${highestBid}` : 'none'}</span></span>
                 <br/>
-                <span className="bid-info">No bids: {numberOfBids}</span>
+                <span className="bid-info">No bids: {bids.length}</span>
                 <br/>
                 <span className="bid-info">Expires at {displayDate(product.endDate)}</span>
             </FormGroup>

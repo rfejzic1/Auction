d92900d45487f0e8e24610ecb92ce018f77d96bc
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-08 15:46:27 +0100

    Home page layout done

--- auction/src/components/Home.js
+++ auction/src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import classNames from 'classnames';
 
 import PageLayout from './PageLayout';
 import Divider from './Common/Divider';
 import Wrapper from './Common/Wrapper';
 import Jumbotron from './Home/Jumbotron';
 import config from '../config';
+import ProductCard from './Shop/ProductCard';
 
 const getProducts = async setProducts => {
     try {
@@ -13,13 +15,71 @@ const getProducts = async setProducts => {
             baseURL: config.API_URL,
             url: '/products'
         });
-
-        console.log(res.data);
         setProducts(res.data);
     } catch (err) {
         console.log(err);
     }
 };
+
+const TiledGallery = ({ products, cols4, limit }) => {
+    const bodyClasses = classNames(
+        'tiled-gallery',
+        { 'cols-4': cols4 }
+    );
+
+    products = products.filter((_, index) => index < limit);
+
+    return (
+        <div className={bodyClasses}>
+            {
+                products.map((product, index) => <ProductCard key={index} product={product} />)
+            }
+        </div>
+    );
+};
+
+const Section = ({ title, products, limit, cols4 }) => {
+    return (
+        <div className='home-section'>
+            <div className="header">
+                {title}
+            </div>
+            <hr/>
+            <TiledGallery products={products} limit={limit} cols4={cols4} />
+        </div>
+    );
+};
+
+const TabbedSection = ({ tabs, limit, cols4 }) => {
+    const [currentTab, setCurrentTab] = useState(Object.keys(tabs)[0]);
+
+    const classes = active => classNames(
+        'tab',
+        { active }
+    );
+
+    return (
+        <div className='home-section'>
+            <div>
+                {
+                    Object.keys(tabs).map((tabName, index) => {
+                        return (
+                            <span
+                                key={index}
+                                onClick={() => setCurrentTab(tabName)}
+                                className={classes(tabName === currentTab)}>
+                                    {tabName}
+                            </span>
+                        )
+                    })
+                }
+            </div>
+            <hr/>
+            <TiledGallery products={tabs[currentTab]} limit={limit} cols4={cols4} />
+        </div>
+    );
+};
+
 
 const Home = () => {
     const [products, setProducts] = useState(null);
@@ -28,12 +88,41 @@ const Home = () => {
         getProducts(setProducts);
     }, []);
 
+    const milisToDays = milis => milis / (1000 * 60 * 60 * 24);
+
+    const filterNewArrivals = products => {
+        const now = Date.now();
+        return products.filter(product => milisToDays(now - product.startDate) <= 2);
+    };
+
+    const filterLastChanceProducts = products => {
+        const now = Date.now();
+        return products.filter(product => milisToDays(product.endDate - now) <= 2);
+    };
+
     return (
         <PageLayout>
-            { products && <Jumbotron product={products[1]} />}
-            <Wrapper>
-                lols
-            </Wrapper>
+            {
+                products
+            &&
+                <>
+                    <Jumbotron product={products[1]} />
+                    <Wrapper>
+                        <Section title='Feature Collection' products={products} limit={3} />
+                        <Divider />
+                        <Section title='Feature Products' cols4 products={products} limit={4} />
+                        <Divider />
+                        <TabbedSection
+                            cols4
+                            limit={4}
+                            tabs={{
+                                'New Arrivals': filterNewArrivals(products),
+                                'Last Chance': filterLastChanceProducts(products)
+                            }}
+                        />
+                    </Wrapper>
+                </>
+            }
             <Divider/>
         </PageLayout>
     )
--- auction/src/components/Home/Jumbotron.js
+++ auction/src/components/Home/Jumbotron.js
@@ -1,21 +1,43 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 import Wrapper from '../Common/Wrapper';
 import CategoryList from './CategoryList';
+import Button from '../Controls/Button';
+
+const randomImage = images => {
+    if (images && images.length <= 0) {
+        return null;
+    }
+
+    const randomIndex = Math.floor(Math.random() * (images.length - 1));
+    return images[randomIndex].uri;
+}
 
 const ProductHighlight = ({ product }) => {
-  const uri = product.images[0].uri;
+  const uri = randomImage(product.images);
   const startPrice = product.startPrice;
 
   return (
         <div className="product-highlight">
+            <div className='details'>
+                <div className='name'>
+                    {product.name}
+                </div>
+                <div className='price'>
+                    {`Starts from $${startPrice.toFixed(2)}`}
+                </div>
+                <div className='description'>
+                    {product.description}
+                </div>
+                <Button className='outlined-primary uppercase'>
+                    Bid Now
+                    <FontAwesomeIcon icon={faChevronRight} />
+                </Button>
+            </div>
             <img src={uri} alt="Shoes"/>
-            <span className='product-name'>
-                {product.name}
-            </span>
-            <span className='product-price'>
-                {`Starts from $${startPrice.toFixed(2)}`}
-            </span>
         </div>
     );
 };

d90c446fec0e602636b0301f48b159d7fa297211
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-20 21:42:30 +0100

    Fixed footer button

--- auction/src/components/Footer/Footer.js
+++ auction/src/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 import { Button, TextField, Social } from '../Common';
@@ -31,10 +30,12 @@ const Footer = () => {
                 <span className={title}>Newsletter</span>
                 <span>Enter your email address and get notified about new products. We hate spam!</span>
                 <TextField  className={textFieldDark} name='newsletter' placeholder='Your Email address'/>
-                <Button fill='dark' outline='primary' fat>
-                    GO
-                    <FontAwesomeIcon icon={faChevronRight} />
-                </Button>
+                <Button 
+                    fill='dark'
+                    outline='primary'
+                    fat
+                    iconRight={faChevronRight}
+                    text='GO'/>
             </Column>
         </footer>
     )

6600c056bb35b44cc7890344e963b9f0bb0d476b
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-22 19:49:29 +0100

    Implemented paging in Shop

--- auction/src/components/Shop/Shop.js
+++ auction/src/components/Shop/Shop.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import axios from 'axios';
 import queryString from 'query-string';
 
-import { Wrapper, Grid, Divider } from '../Common';
+import { Wrapper, Grid, Divider, Button } from '../Common';
 import PageLayout from '../PageLayout'
 import SubcategoryList from './SubcategoryList';
 import ProductCard from './ProductCard';
 
 import config from '../../config';
 
-import { shop } from './Shop.module.scss';
+import { shop, noProductsMessage } from './Shop.module.scss';
 
-const getProducts = async ({ category, subcategory }) => {
+const PAGE_SIZE = 9;
+
+const getProducts = async ({ category, subcategory, page }) => {
     try {    
         const res = await axios({
             baseURL: config.API_URL,
             url: `/products`,
             params: {
                 category,
-                subcategory
+                subcategory,
+                page,
+                size: PAGE_SIZE
             }
         })
         return res.data;
@@ -28,29 +32,76 @@ const getProducts = async ({ category, subcategory }) => {
     }
 };
 
-const Shop = () => {
-    const location = useLocation();
-    const { category, subcategory, search } = queryString.parse(location.search);
+const ProductsView = ({ products, canLoadNewPage, loadNextPage }) => {
+    if (products.length > 0) {
+        return (
+            <>
+                <Grid>
+                    {products.map((product, index) => {
+                        return <ProductCard key={index} product={product} />
+                    })}
+                </Grid>
+                <Divider />
+                <Wrapper flex around>
+                    {
+                        canLoadNewPage 
+                        && 
+                        <Button
+                            onClick={loadNextPage}
+                            text='EXPLORE MORE'
+                            fill='primary'
+                            outline='none'/>
+                    }
+                </Wrapper>
+            </>
+        );
+    }
 
+    return (
+        <>
+            <Divider smaller/>
+            <Wrapper flex around>
+                <span className={noProductsMessage}>
+                    No products found...
+                    <Link to='/shop'>
+                        Back to all categories?
+                    </Link>
+                </span>
+            </Wrapper>
+        </>
+    );
+} 
+
+const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [canLoadNewPage, setCanLoadNewPage] = useState(true);
+
+    const location = useLocation();
+    const { category, subcategory } = queryString.parse(location.search);
 
     useEffect(() => {
-        getProducts({ category, subcategory })
-            .then(products => {
-                if (search) {
-                    const regexString = search
-                        .split(',')
-                        .map(word => `${word}`)
-                        .join('|');
-                    
-                    const regex = new RegExp(regexString, 'gi');
-                    products = products.filter(product => product.name.match(regex));
-                }
-
-                setProducts(products);
+        getProducts({ category, subcategory, page: currentPage })
+            .then(loadedProducts => {
+                setCanLoadNewPage(loadedProducts.length >= PAGE_SIZE);
+                setProducts([...products, ...loadedProducts]);
             })
             .catch(err => console.log(err));
-    }, [category, subcategory, search]);
+    }, [category, subcategory, currentPage]);
+    
+    useEffect(() => {
+        getProducts({ category, subcategory })
+            .then(loadedProducts => {
+                setProducts(loadedProducts);
+            })
+            .catch(err => console.log(err));
+    }, [category, subcategory]);
+
+    const loadNextPage = () => {
+        if (canLoadNewPage) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
 
     return (
         <PageLayout>
@@ -61,13 +112,10 @@ const Shop = () => {
                         <SubcategoryList defaultCategory={category}/>
                     </aside>
                     <main>
-                        <Grid>
-                            {
-                                products.map((product, index) => {
-                                    return <ProductCard key={index} product={product} />
-                                })
-                            }
-                        </Grid>
+                        <ProductsView
+                            products={products}
+                            canLoadNewPage={canLoadNewPage}
+                            loadNextPage={loadNextPage}/>
                     </main>
                 </div>
             </Wrapper>

21a4023c3f9203eecacfc9f6ad898fce790b42bc
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-03-30 17:40:14 +0200

    Implemented ordering

--- auction/src/components/Shop/Shop.js
+++ auction/src/components/Shop/Shop.js
@@ -3,18 +3,24 @@ import { useLocation, Link } from 'react-router-dom';
 import axios from 'axios';
 import queryString from 'query-string';
 
-import { Wrapper, Grid, Divider, Button } from '../Common';
+import { Wrapper, Grid, Divider, Button, SelectField } from '../Common';
 import PageLayout from '../PageLayout'
 import SubcategoryList from './SubcategoryList';
 import ProductCard from './ProductCard';
 
 import config from '../../config';
 
-import { shop, noProductsMessage } from './Shop.module.scss';
+import { 
+    shop,
+    noProductsMessage,
+    viewBar as viewBarClass
+} from './Shop.module.scss';
 
 const PAGE_SIZE = 9;
 
-const getProducts = async ({ category, subcategory, page }) => {
+// FIX LOADING ERROR.....
+
+const getProducts = async ({ category, subcategory, page, orderBy }) => {
     try {    
         const res = await axios({
             baseURL: config.API_URL,
@@ -23,7 +29,8 @@ const getProducts = async ({ category, subcategory, page }) => {
                 category,
                 subcategory,
                 page,
-                size: PAGE_SIZE
+                size: PAGE_SIZE,
+                orderBy
             }
         })
         return res.data;
@@ -31,6 +38,30 @@ const getProducts = async ({ category, subcategory, page }) => {
         return [];
     }
 };
+
+const ViewBar = ({ onOrderByChange }) => {
+    const orderByOptions = [
+        {
+            name: 'Sort by name',
+            value: 'NAME'
+        },
+        {
+            name: 'Sort by newness',
+            value: 'NEWEST'
+        },
+        {
+            name: 'Sort by price',
+            value: 'PRICE_LOWEST'
+        }
+    ];
+    
+    return (
+        <div className={viewBarClass}>
+            <SelectField options={orderByOptions} onChange={onOrderByChange}/>
+
+        </div>
+    );
+}
 
 const ProductsView = ({ products, canLoadNewPage, loadNextPage }) => {
     if (products.length > 0) {
@@ -76,31 +107,28 @@ const Shop = () => {
     const [products, setProducts] = useState([]);
     const [currentPage, setCurrentPage] = useState(0);
     const [canLoadNewPage, setCanLoadNewPage] = useState(true);
+    const [orderBy, setOrderBy] = useState('');
 
     const location = useLocation();
     const { category, subcategory } = queryString.parse(location.search);
 
     useEffect(() => {
-        getProducts({ category, subcategory, page: currentPage })
+        getProducts({ category, subcategory, page: currentPage, orderBy })
             .then(loadedProducts => {
                 setCanLoadNewPage(loadedProducts.length >= PAGE_SIZE);
-                setProducts([...products, ...loadedProducts]);
+                setProducts([...loadedProducts]);
             })
             .catch(err => console.log(err));
-    }, [category, subcategory, currentPage]);
-    
-    useEffect(() => {
-        getProducts({ category, subcategory })
-            .then(loadedProducts => {
-                setProducts(loadedProducts);
-            })
-            .catch(err => console.log(err));
-    }, [category, subcategory]);
+    }, [category, subcategory, currentPage, orderBy]);
 
     const loadNextPage = () => {
         if (canLoadNewPage) {
             setCurrentPage(currentPage + 1);
         }
+    };
+
+    const handleOrderByChange = ({ value }) => {
+        setOrderBy(value);
     };
 
     return (
@@ -112,6 +140,7 @@ const Shop = () => {
                         <SubcategoryList defaultCategory={category}/>
                     </aside>
                     <main>
+                        <ViewBar onOrderByChange={handleOrderByChange}/>
                         <ProductsView
                             products={products}
                             canLoadNewPage={canLoadNewPage}
